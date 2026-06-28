aa82818274e8d4066091605c735ed0ea005c6b92
Anindya Sarkar <sanindya710@gmail.com>
2026-06-28 10:42:41 +0530

    Solved the conflict

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -2,11 +2,14 @@ import Singup from './auth/Signup/signup.jsx';
 import { Routes,Route } from 'react-router-dom';
 
 export default function App() {
-  return (
-    
+  return (<>
+    <h1 className="text-3xl font-bold underline bg-amber-300">
+      Hello world
+    </h1>
+  
       <Routes>
         <Route path="/signup" element={<Singup />} />
       </Routes>
     
-  );
+  </>);
 }
